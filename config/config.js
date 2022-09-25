ddb3d0ae486875e0a742b5edd8b250b1ab93fc82
Carolina <carolinazapatavidal@gmail.com>
2022-09-25 12:44:09 -0300

    configg

---
+++ config/config.js
@@ -0,0 +1,8 @@
+
+  const username = "bsale_test";
+  const password = "bsale_test";
+  const database = "bsale_test";
+  const host = "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com";
+  const dialect =  "mysql";
+
+  module.exports = {username, password, database, host, dialect}
