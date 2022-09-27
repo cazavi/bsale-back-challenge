085bcb76d412308f06e9c51250ef3befbca5f92b
Carolina Zapata <102156891+cazavi@users.noreply.github.com>
2022-09-27 07:54:18 -0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-  const username = "bsale_test";
-  const password = "bsale_test";
-  const database = "bsale_test";
-  const host = "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com";
-  const dialect =  "mysql";
-
-  module.exports = {username, password, database, host, dialect}
